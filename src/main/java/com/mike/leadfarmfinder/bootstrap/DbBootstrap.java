416b7c2f0d3ab301589b1ccd3c81f6f2346aee83
michalswig <mike@data.pl>
2025-12-05 18:35:31 +0100

    refactor

--- src/main/java/com/mike/leadfarmfinder/bootstrap/DbBootstrap.java
+++ src/main/java/com/mike/leadfarmfinder/bootstrap/DbBootstrap.java
@@ -16,7 +16,7 @@ import java.time.LocalDateTime;
 import java.util.HashSet;
 import java.util.Set;
 
-@Component
+//@Component
 @RequiredArgsConstructor
 @Slf4j
 public class DbBootstrap implements CommandLineRunner {
