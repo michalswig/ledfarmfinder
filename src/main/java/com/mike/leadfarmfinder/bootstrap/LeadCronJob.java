2a9308ffceaddd6e899de686e8db7e83d1ddb9b1
michalswig <mike@data.pl>
2026-04-25 13:33:35 +0200

    crons enabled

--- src/main/java/com/mike/leadfarmfinder/bootstrap/LeadCronJob.java
+++ src/main/java/com/mike/leadfarmfinder/bootstrap/LeadCronJob.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Component;
 import java.time.LocalDateTime;
 import java.util.List;
 
-//@Component
+@Component
 @Slf4j
 @RequiredArgsConstructor
 public class LeadCronJob {
